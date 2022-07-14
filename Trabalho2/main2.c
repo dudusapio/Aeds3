4eb57f6267da0ad4766f753a8559676c50d7cd93
Eduardo Sapio Saccardo <dudusapio@live.com>
2022-07-14 19:28:06 -0300

    main2.c pronto

--- Trabalho2/main2.c
+++ Trabalho2/main2.c
@@ -9,6 +9,21 @@ void floydWarshall(int quantVertices);
 void printSolution(int quantVertices);
 void verticesMaior(int quantVertices);
 
+//Dijkstra
+void CriaVetoresAux(int quantVertices);
+void Inicializa(int s,int quantVertices);
+void Relaxa(int chave, int ligado, int peso);
+void Dijskstra(int partida,int destino,int quantVertices);
+int extrairMinimo(int quantVertices);
+
+int * distancia;
+int * pai;
+
+int * conjQ; // Distância provisória
+int * conjS; // Distancia mínima garantida
+
+//
+
 typedef struct {
     int chave;
     int ligado;
@@ -31,7 +46,7 @@ int main (void){
     int quantVertices,numeroLinhas = 0,i=0,j = 0;
 
     FILE *p;
-    p = fopen("n25.txt","r");
+    p = fopen("n1000.txt","r");
     if(p == NULL){
         printf("ERRO! O arquivo nao foi aberto!\n");
         exit(100);
@@ -88,9 +103,20 @@ int main (void){
         verticesMaior(quantVertices);
         printf("vertice1 = %d\n",vertice1);
         printf("vertice2 = %d\n",vertice2);
-        printf("peso = %d\n",dist[vertice1][vertice2]);
+        
         //2. Rodar um dijstra a partir de algum desse vértice até o outro vértice.
+        Dijskstra(vertice1,vertice2,quantVertices);
         //3. Printar os pais
+        // for(int i = 0 ; i < quantVertices;i++){
+        //     printf("distancia[%2d] = %7d | pai[%2d] = %7d\n",i,distancia[i],i,pai[i]);
+        // }
+        int x = vertice2;
+        printf("%d ",vertice2);
+        while(x != vertice1){
+            printf("%d ",pai[x]);
+            x = pai[x];
+        }
+        printf("\npeso = %d",dist[vertice1][vertice2]);
     }
 }
 
@@ -126,7 +152,7 @@ void floydWarshall(int quantVertices){
             }
         }
     }
-    printSolution(quantVertices);
+    //printSolution(quantVertices);
 }
 
 void printSolution(int quantVertices){
@@ -152,4 +178,77 @@ void verticesMaior(int quantVertices){
             }
         }
     }
+}
+
+//Dijkstra
+
+//Funcionando para o n1000.txt
+void CriaVetoresAux(int quantVertices){
+    distancia = (int *) malloc(quantVertices * sizeof(int));
+    pai = (int *) malloc(quantVertices * sizeof(int));
+    conjQ = (int *) malloc (quantVertices * sizeof(int));
+    conjS = (int *) malloc (quantVertices * sizeof(int));
+}
+//Funcionando para o n1000.txt
+void Inicializa(int s,int quantVertices){
+    for(int i = 0; i < quantVertices;i++){
+        distancia[i] = 9999;
+        pai[i] = 9999;
+        conjQ[i] = 1; //Setando todos como verdadeiro(1)
+        conjS[i] = 0; //Setando todos como falso(0), pois ainda não conhecemos a menor distância de nenhum vértice
+    }
+    distancia[s] = 0;
+}
+
+void Relaxa(int chave, int ligado, int peso){
+    if(distancia[ligado] > (distancia[chave] + peso)){
+        distancia[ligado] = distancia[chave] + peso;
+        pai[ligado] = chave;
+    }
+}
+
+// void Dijskstra(int partida,int destino,int quantVertices){
+//     int u = 0,j;
+//     CriaVetoresAux(quantVertices);
+//     Inicializa(vertice1,quantVertices);
+//     while(conjS[vertice2] != 1){ // Ou seja, enquanto não sabemos a menor distância garantida do vertice 2 continuamos executar
+//         u = extrairMinimo(quantVertices);
+//         for(j = 0; j < quantVertices ; j++){
+//             if(mat[partida][j] != 99999 || mat[partida][j] != 0){
+//                 Relaxa(partida,j,mat[partida][j]);
+//             }
+//         }
+//     }
+// }
+
+void Dijskstra(int partida,int destino,int quantVertices){
+    int u = 0,i,j;
+    CriaVetoresAux(quantVertices);
+    Inicializa(vertice1,quantVertices);
+    while(conjS[vertice2] != 1){ // Ou seja, enquanto não sabemos a menor distância garantida do vertice 2 continuamos executar
+        u = extrairMinimo(quantVertices);
+        for(i = 0; i < quantVertices; i++){
+            for(j = 0; j < quantVertices ; j++){
+                if(mat[i][j] != 99999 || mat[partida][j] != 0){
+                    Relaxa(i,j,mat[i][j]);
+                }
+            }
+        }
+    }
+}
+
+
+int extrairMinimo(int quantVertices){
+    int minimo = 99999;
+    int verticeMinimo;
+    for(int i = 0; i < quantVertices;i++){
+        if(conjQ[i] == 1 && distancia[i] < minimo){
+            minimo = distancia[i];
+            verticeMinimo = i;
+        }
+    }
+    conjQ[verticeMinimo] = 0;
+    conjS[verticeMinimo] = 1;
+    count++;
+    return verticeMinimo;
 }
